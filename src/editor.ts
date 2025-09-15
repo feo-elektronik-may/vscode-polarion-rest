dbbb30c0bd0eabb120971715f9fb1fd24940d638
Johannes May <may@feo-elektronik.de>
2025-09-15 14:51:48 +0200

    Remove HTML to markdown conversion
    Newer VS Code versions allow a subset of HTML tags in markdown, this should be good enough to render Polarion WorkItems.

--- src/editor.ts
+++ src/editor.ts
@@ -2,9 +2,6 @@ import { resolve } from 'dns';
 import * as vscode from 'vscode';
 import * as pol from './polarion';
 import * as utils from './utils';
-// import * as cleanhtml from 'clean-html';
-// import * as turndown from 'turndown';
-// import TurndownService = require('turndown');
 
 const decorationType = vscode.window.createTextEditorDecorationType({});
 const open = require('open');
@@ -41,91 +38,24 @@ export async function decorate(editor: vscode.TextEditor) {
   editor.setDecorations(decorationType, decorationsArray);
 }
 
-export async function buildHoverMarkdown(workItem: string): Promise<string[]> {
+export async function buildHoverMarkdown(workItem: string): Promise<vscode.MarkdownString[]> {
   let item = await pol.polarion.getWorkItem(workItem);
   let url = await pol.polarion.getUrlFromWorkItem(workItem);
-  let hover: string[] = [];
+  let hover: vscode.MarkdownString[] = [];
   if (item !== undefined) {
-    hover.push(`${workItem} (${item.type.id}) ***${item.title}***  \nAuthor: ${item.author.id}  \n Status: ${item.status.id}`);
+    hover.push(new vscode.MarkdownString(`${workItem} (${item.type.id}) ***${item.title}***  \nAuthor: ${item.author.id}  \n Status: ${item.status.id}`));
     if (item.description) {
-
-      hover.push(await getMkDiscription(item.description?.content));
+      let content = new vscode.MarkdownString(item.description?.content);
+      content.supportHtml = true;
+      hover.push(content);
     }
-    hover.push(`[Open in Polarion](${url})`);
+    hover.push(new vscode.MarkdownString(`[Open in Polarion](${url})`));
   }
   else {
-    hover.push(`Not found`);
+    hover.push(new vscode.MarkdownString(`Not found`));
   }
   return hover;
 }
-
-async function getMkDiscription(des: string): Promise<string> {
-  return new Promise((resolve, reject) => {
-
-    let html2md = require('html-to-md');
-    let strippedHtml = des.replace(/(\r\n|\n|\r)/gm, "");
-
-    //cleanup html sources
-    var cleaner = require('clean-html');
-    var options = {
-      'add-remove-tags': ['div', 'span', 'br', 'tbody', 'p'],
-      'add-remove-attributes': ['style']
-    };
-    cleaner.clean(strippedHtml, options, function (html: string) {
-      var html2json = require('html2json').html2json;
-      var json2html = require('html2json').json2html;
-      let json: any = html2json(html);
-
-      //TODO: fix tables in children
-      for (let n of json['child']) {
-        if (n.tag === 'table') {
-          n = fixTableHeader(n);
-        }
-      }
-
-
-      html = json2html(json);
-      let md = html2md(html);
-      resolve(md);
-    });
-
-
-
-
-
-    // return md;
-  });
-
-}
-
-function fixTableHeader(json: any): any {
-  let rowCnt = 0;
-  let thead: { node: string, tag: string, child: any[] } = { node: 'element', tag: 'thead', child: [] };
-  let tbody: { node: string, tag: string, child: any[] } = { node: 'element', tag: 'tbody', child: [] };
-
-
-  for (let c of json['child']) {
-    console.log(c);
-    if (c['tag'] === 'tr') {
-      let tr = c;
-      if (rowCnt === 0) {
-
-        thead.child.push(tr);
-      }
-      else {
-        tbody.child.push(tr);
-      }
-      rowCnt++;
-    }
-
-  }
-
-  json['child'] = [];
-  json['child'].push(thead);
-  json['child'].push(tbody);
-  return json;
-}
-
 
 export async function handleOpenPolarion() {
   const editor = vscode.window.activeTextEditor;
